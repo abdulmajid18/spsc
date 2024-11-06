47673606d4472ce2b9a0d08e9663543048073ea8
abdulmajid18 <nuhuabdulmajid58@gmail.com>
2024-11-06 00:05:04 +0000

    added pop operation

--- include/spsc/impl/circular_buffer.impl.cpp
+++ include/spsc/impl/circular_buffer.impl.cpp
@@ -7,6 +7,36 @@ template<typename T, size_t Size>
 CircularBuffer<T, Size>::CircularBuffer() : head(0), tail(0), full(false) {}
 
 template<typename T, size_t Size>
+T CircularBuffer<T, Size>::pop(unsigned max_attempts = 3) {
+    unsigned backoff = MIN_BACKOFF; // Initial small backoff
+
+    for (unsigned attempt = 0; attempt < max_attempts; ++attempt) {
+        size_t current_head = head.load(std::memory_order_acquire);
+        size_t current_tail = tail.load(std::memory_order_relaxed);
+        bool is_full = full.load(std::memory_order_relaxed);
+
+        if (!is_full && current_head == current_tail) {
+            // Buffer is empty, apply backoff and retry
+            backoff = std::min(backoff * 2, MAX_BACKOFF);  // Exponential backoff
+            std::this_thread::sleep_for(std::chrono::microseconds(backoff));
+            continue;
+        }
+
+        item = std::move(buffer[current_tail]);
+
+        size_t new_tail = (current_tail + 1) & MASK;
+        tail.store(new_tail, std::memory_order_release);
+
+        full.store(false, std::memory_order_release);
+
+        return true; 
+    }
+
+    return false;  
+}
+
+
+template<typename T, size_t Size>
 bool CircularBuffer<T, Size>::push(T item, unsigned max_attempts = 3) {
     unsigned backoff = MIN_BACKOFF;
 
